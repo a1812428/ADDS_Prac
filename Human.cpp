680a6d2e6a9e0a4bbf050660303129f1111a712a
Jyotin Shivampeta <a1812428@adelaide.edu.au>
2022-08-04 23:03:22 +0930

    solutions

--- Human.cpp
+++ Human.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 Human::Human()
 {
-    cin >> this->move;
+    // cin >> this->move;
 }
 
 char Human::makeMove()
 {
-    // cin >> this->move;
+    cin >> this->move;
     return this->move;
 }
